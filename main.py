c5ee6c1a80f80545f5a5854fc93438b3992c074a
Soy Miguel <magutierrezhijosa@gmail.com>
2026-03-03 21:31:21 +0100

    Recogida de datos en un diccionario

--- main.py
+++ main.py
@@ -41,61 +41,79 @@ with sync_playwright() as p:
     # Le decimos que espere a que terrmine de cargar todo
     page.wait_for_load_state("networkidle")
 
-    # Recogemos todos los items que vamos a scrapear
-    # Mucho mejor usar locator() que query() REECOMENDACION Playwright
-    items = page.locator("div.views-row")
-
-    #Compruebo si me seleciona todas las publicaciones 
-    print("Publicaiones encontradas", items.count())
-
     # Los datos que deseamos recoger de la pagina 
     # 1.TITULO
     # 2.FECHA
     # 3.PDF
+
+    # Creamos la funcion para realizar el scrapp
+    def scrapear_publicaciones(page):   
+
+        # Defino una varaible diccionario para guardar los daatos y posteriormente enviarlos a un CSV
+        results = []
+
+         # Recogemos todos los items que vamos a scrapear
+        # Mucho mejor usar locator() que query() REECOMENDACION Playwright
+        items = page.locator("div.views-row")
+
+        #Compruebo si me seleciona todas las publicaciones 
+        print("Publicaiones encontradas", items.count())
+
+        # Creamos un bucle para iterar las diferentes publicaciones
+        for i in range(items.count()):
+
+            # Llamamos a la funcion nth() para seleccionar un elemento concreto dentro de un conjunto que comparten un locator()
+            item = items.nth(i)
+
+            # Declaramos la variable que va a recoger el TITULO
+            # Utilizamos get_by_role() por que es el selector mas estable y robusto no depende del HTML y resiste cambios CSS
+            title = item.get_by_role("link").first.inner_text()
+
+            ########### Mostramos los titulos #############
+            print("Titulo de la publicacion : " , title)
+
+            # Vamos a buscar dentro del elemento item el primer <p> que encuentre
+            date_element = item.locator("p.text-body.mt-1") 
+        
+            # Recogemos la data del elemento seleccionado anteriormente
+            date_text = date_element.first.text_content()
+
+            # Ahora vamos a recoger solamente el valor del año utilizando Expresiones Regulares
+            match = re.search(r"\b20\d{2}\b",date_text)
+
+            # Transformamos el texto para guardarlo en la variable final en el caso de que lo haya encontrado 
+            year = match.group() if match else None
+
+        
+            ########### Mostramos los FECHA #############    
+            print("Esta es la fecha del documento : " , year)
+
+            # Marcamos el elemento que vamos a sacar la informacion 
+            meta_link = item.locator("a.unido-link.link")
+        
+                
+            # Recogemos el texto del link utilizando la referencia al "href" y lo guardamos
+            href = meta_link.first.get_attribute("href")
+
+            # Unimos la Url base mas el href reelativo 
+            pdf_link = urljoin(URL_BASE,href) if href else None
+                
+            ########### Mostramos el LINK #############    
+            print("Esto es el link del PDF : " , pdf_link)
+
+            # Guardamos los datos dentro del diccionario 
+            results.append({
+                "title": title,
+                "year": year,
+                "link":pdf_link
+            })
+
+             # Cerramos el navegador
+        browser.close()
     
-    # Creamos un bucle para iterar las diferentes publicaciones
-    for i in range(items.count()):
+        return results
 
-        # Llamamos a la funcion nth() para seleccionar un elemento concreto dentro de un conjunto que comparten un locator()
-        item = items.nth(i)
-
-        # Declaramos la variable que va a recoger el TITULO
-        # Utilizamos get_by_role() por que es el selector mas estable y robusto no depende del HTML y resiste cambios CSS
-        title = item.get_by_role("link").first.inner_text()
-
-        ########### Mostramos los titulos #############
-        print("Titulo de la publicacion : " , title)
-
-        # Vamos a buscar dentro del elemento item el primer <p> que encuentre
-        metadata = item.locator("p.text-body.mt-1")
-
-        # Comprobamos que haya elementos 
-       
-        # Recogemos la data del elemento seleccionado anteriormente
-        date_text = metadata.first.text_content()
-
-        # Ahora vamos a recoger solamente el valor del año utilizando Expresiones Regulares
-        match = re.search(r"\b20\d{2}\b",date_text)
-
-        # Transformamos el texto para guardarlo en la variable final en el caso de que lo haya encontrado 
-        year = match.group() if match else None
-
-       
-        ########### Mostramos los FECHA #############    
-        print("Esta es la fecha del documento : " , year)
-
-        # Marcamos el elemento que vamos a sacar la informacion 
-        meta_link = item.locator("a.unido-link.link")
-       
-            
-        # Recogemos el texto del link utilizando la referencia al "href" y lo guardamos
-        href = meta_link.first.get_attribute("href")
-
-        # Unimos la Url base mas el href reelativo 
-        pdf_link = urljoin(URL_BASE,href) if href else None
-            
-        ########### Mostramos el LINK #############    
-        print("Esto es el link del PDF : " , pdf_link)
-
-    # Cerramos el navegador
-    browser.close()
+    if __name__ == "__main__":
+        results = scrapear_publicaciones(page)
+    print(results)
+    
